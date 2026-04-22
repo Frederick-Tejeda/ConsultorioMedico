6382450e901defceecc84e7d748588ac0aa138ad
Frederick-Tejeda <103900166+Frederick-Tejeda@users.noreply.github.com>
2026-04-22 17:58:18 -0400

    Trying something different...

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const helmet = require("helmet");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://interfaz-core.vercel.app/"); 
+  res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   
